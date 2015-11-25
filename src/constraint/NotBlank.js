fa48610e169c0d5bfc10c08e47ba82a95ff1c505
Hugo Henrique <contato@hugohenrique.com.br>
2015-11-25 15:29:07 -0300

    Removed duplicated files

--- src/constraint/NotBlank.js
+++
@@ -1,9 +0,0 @@
-Validator.addConstraint(
-  'notBlank',
-  'Should not be blank',
-  function () {
-    var value = this.record[this.attribute];
-
-    return !!value && !!value.replace(/\s/mg, '');
-  }
-);
