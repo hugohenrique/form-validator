4701d91e08f6a024913c65cf6b89fd0c501c67db
Hugo Henrique <contato@hugohenrique.com.br>
2019-05-22 20:13:30 -0300

    Changed ER to email constraint

--- src/constraint/email.js
+++ src/constraint/email.js
@@ -3,7 +3,6 @@ Validator.addConstraint(
   'Isn\'t a valid email address',
   function () {
     var value = this.record[this.attribute] || '';
-
-    return !!value.match(/^[a-z0-9]+([._][a-z0-9]+)*(\+[a-z0-9_-]+)?@[a-z0-9]+([.-][a-z0-9]+)*\.[a-z]{2,4}$/i);
+    return !!value.match(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
   }
 );
